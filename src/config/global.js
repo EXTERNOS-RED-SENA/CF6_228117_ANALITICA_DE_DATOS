e27e680cd76b72fd99b0a84557b4dc8ae832a4da
elrilacho <camilobr17@gmail.com>
2024-11-24 15:54:00 -0500

    Sintesis cargada

--- src/config/global.js
+++ src/config/global.js
@@ -183,7 +183,7 @@ export default {
       {
         icono: 'fas fa-file-pdf',
         titulo: 'Descargar PDF',
-        download: 'downloads/prueba.pdf',
+        download: 'downloads/228117_CF06_DU.pdf',
       },
       {
         icono: 'fas fa-download',
